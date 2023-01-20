6c8e7c4282aa7704e314bd82a1974310cd80f4ca
Prashant Gupta <prashant.gupta@tothenew.com>
2023-01-20 18:22:37 +0530

    Updated VPC peering when using different account

--- example/within-account/main.tf
+++ example/within-account/main.tf
@@ -1,6 +1,16 @@
 # VPC Peering within AWS account
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "current-profile-name"
+  # Requester's credentials.
+}
+
 module "peering_within_account" {
   source           = "git::https://github.com/tothenew/terraform-aws-vpc-peering.git"
   accepter_vpc_id  = "vpc-111111111111"
   requester_vpc_id = "vpc-999999999999"
+  providers = {
+    aws.this = aws
+    aws.peer = aws
+  }
 }
