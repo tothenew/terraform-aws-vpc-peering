634ca6004118af2f5030a3f3978ebac9c200eee3
Prashant Gupta <prashant.gupta@tothenew.com>
2023-01-20 17:17:26 +0530

    Added provider passing through variable

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_region" "current" {}
 # VPC peering connection #
 ##########################
 resource "aws_vpc_peering_connection" "vpc_peering_connection" {
-  provider      = aws.this
+  provider      = var.requester_provider
   peer_owner_id = var.accepter_owner_id != 0 ? var.accepter_owner_id : data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.accepter_vpc_id
   vpc_id        = var.requester_vpc_id
@@ -18,7 +18,7 @@ resource "aws_vpc_peering_connection" "vpc_peering_connection" {
 # VPC peering accepter configuration #
 ######################################
 resource "aws_vpc_peering_connection_accepter" "peer_accepter" {
-  provider                  = aws.peer
+  provider                  = var.accepter_provider
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering_connection.id
   auto_accept               = var.auto_accept_peering ? var.auto_accept_peering : (var.accepter_region == "" ? true : var.auto_accept_peering)
   tags                      = merge(var.common_tags, tomap({ "Name" : "${var.project_name_prefix}-${var.app_name}" }))
@@ -28,7 +28,7 @@ resource "aws_vpc_peering_connection_accepter" "peer_accepter" {
 # VPC peering options #
 #######################
 resource "aws_vpc_peering_connection_options" "requester_dns_resolution" {
-  provider                  = aws.this
+  provider                  = var.requester_provider
   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer_accepter.id
 
   requester {
@@ -37,7 +37,7 @@ resource "aws_vpc_peering_connection_options" "requester_dns_resolution" {
 }
 
 resource "aws_vpc_peering_connection_options" "accepter_dns_resolution" {
-  provider                  = aws.peer
+  provider                  = var.accepter_provider
   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer_accepter.id
 
   accepter {
