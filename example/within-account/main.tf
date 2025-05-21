74bea3974c49331b60825ff19486433f076b8d8b
dhruvguptaTTN <dhruv.gupta@tothenew.com>
2025-05-21 10:18:50 +0530

    Update main.tf

--- example/within-account/main.tf
+++ example/within-account/main.tf
@@ -1,14 +1,14 @@
 # VPC Peering within AWS account
 provider "aws" {
   region  = "ap-east-2"
-  profile = "Dhruv Gupta"
+  profile = "ABC"
   # Requester's credentials.
 }
 
 module "peering_within_account" {
   source           = "git::https://github.com/tothenew/terraform-aws-vpc-peering.git"
-  accepter_vpc_id  = "vpc-09aceeb53e4bd8994"
-  requester_vpc_id = "vpc-00da5792f9cb19678"
+  accepter_vpc_id  = "vpc-009988776544"
+  requester_vpc_id = "vpc-009988776655"
   providers = {
     aws.this = aws
     aws.peer = aws
